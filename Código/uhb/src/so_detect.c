8b58d37e3afd159d121813198c8071453f37f1cd
jpmartinsdacosta <jpmdacosta10@gmail.com>
2025-02-02 19:26:43 +0100

    Makefile arreglado tras instalar gcc, Debian aún no funciona

--- Código/uhb/src/so_detect.c
+++ Código/uhb/src/so_detect.c
@@ -3,25 +3,30 @@
 #include <string.h>
 #include "so_detect.h"
 
-int so_detect(){
+int so_detect() {
     FILE *fp;
     char path[1035];
-    char *os;
+    char *newline;
     int os_id = -1;
 
     // Open the command for reading.
     fp = popen("uname -s", "r");
     if (fp == NULL) {
-        printf("Failed to run command\n" );
+        printf("Failed to run command\n");
         return -1;
     }
 
     // Read the output a line at a time - output it.
-    while (fgets(path, sizeof(path)-1, fp) != NULL) {
-        os = strtok(path, "\n");
-        if(strcmp(os, "FreeBSD") == 0){
+    while (fgets(path, sizeof(path) - 1, fp) != NULL) {
+        printf("Output from uname: %s", path); // Debug print
+        newline = strchr(path, '\n');
+        if (newline != NULL) {
+            *newline = '\0';
+        }
+        printf("OS detected: %s\n", path); // Debug print
+        if (strcmp(path, "FreeBSD") == 0) {
             os_id = 0;
-        } else if(strcmp(os, "Linux") == 0){
+        } else if (strcmp(path, "Linux") == 0) {
             os_id = 1;
         }
     }
