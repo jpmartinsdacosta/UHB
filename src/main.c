6572294018dda70680a60c8512e40a6370112cd8
jpmartinsdacosta <jpmdacosta10@gmail.com>
2025-02-05 09:34:31 +0100

    [uhb_0.1] Successfully discriminate between OS when compiling.

--- src/main.c
+++ src/main.c
@@ -5,40 +5,48 @@
 #ifdef _WIN32
 #include <io.h>
 #include <windows.h>
-#else
+#elif defined(__FreeBSD__)
 #include <unistd.h>
+#include "imp_bsd.h"
+#elif defined(__linux__)
+#include <unistd.h>
+#include "imp_deb.h"
 #endif
 #include "so_detect.h"
-#include "imp_bsd.h"
-#include "imp_deb.h"
 
 int main() {
     int os = 0;
-    int deb, bsd;
+    int deb = 0, bsd = 0;
+
     // Check if UHB is being executed by the root user.
     // TODO: Check if UHB is being executed by the uhb user, using the getpwnam function.
-    if(getuid() != 0) {
+    if (getuid() != 0) {
         printf("UHB must be executed by the root user\n");
         return -1;
     }
 
     // Detect the operating system and execute its respective implementation.
-     
     switch (os = so_detect()) {
         case -1:
             printf("Failed to detect or unknown operating system\n");
             return 1;
         case 0:
             printf("FreeBSD\n");
+            #ifdef __FreeBSD__
             bsd = imp_bsd();
+            #endif
             break;
         case 1:
             printf("Debian\n");
+            #ifdef __linux__
             deb = imp_deb();
+            #endif
             break;
     }
+
     if (bsd == 1 || deb == 1) {
         printf("Success\n");
     }
+
     return 0;
 }
